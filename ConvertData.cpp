f4c35dd48891c09c31cbe46478ea3fbfe9e514fe
foreverYoungGitHub <yliu344@uottawa.ca>
2016-10-08 16:54:37 -0400

    The basic framework

--- ConvertData.cpp
+++ ConvertData.cpp
@@ -5,85 +5,277 @@
 #include "ConvertData.h"
 
 
-void rect_read()
+ConvertData::ConvertData()
 {
-    for(int i=0; i<file_list_.size(); i++)
-    {
-        string rec_read_path = path + file_list_[i] + ".txt";
-        vector<Rect> cur_rects;
-        string line;
-        Rect cur_rect;
-        ifstream rec_read(rec_read_path);
-        while(!rec_read.eof())
-        {
-            getline(rec_read, line);
-            if(line == "")
-                continue;
-            cur_rect = convert_rect(line);
-            cur_rects.push_back(cur_rect);
-        }
-        rect_.push_back(cur_rects);
-    }
+
 }
 
-void num_read(string headpath)
+ConvertData::ConvertData(std::vector<string> params)
 {
-    for(int i=0; i<file_list_.size(); i++)
-    {
-        string num_read_path = path + file_list_[i] + "_head_num.txt";
-        vector<int> cur_nums;
-        string line;
-        int cur_num;
-        ifstream num_read(num_read_path);
-        while(!num_read.eof())
-        {
-            getline(num_read, line);
-            if(line == "")
-                continue;
-            cur_num = convert_num(line);
-            cur_nums.push_back(cur_num);
-
-        }
-        num_.push_back(cur_nums);
-    }
+    params_ = params;
 }
 
-void img_path_read(string headpath)
+ConvertData::~ConvertData()
 {
-    for(int i=0; i<file_list_.size(); i++)
-    {
-        string img_read_path = headpath + file_list_[i] + "_img_path.txt";
-        vector<string> cur_img_pathes;
-        string line;
-        ifstream img_path_read(img_read_path);
-        while(!img_path_read.eof())
-        {
-            getline(img_path_read, line);
-            if(line == "")
-                continue;
-            cur_img_pathes.push_back(line);
-        }
-        path_.push_back(cur_img_pathes);
-    }
+
 }
 
-
-bool ConvertData::img_path_read(string headpath, int i)
+bool ConvertData::run()
 {
-    string img_read_path = headpath + file_list_[i] + "_img_path.txt";
-    vector<string> cur_img_pathes;
-    string line;
-    ifstream img_path_read(img_read_path);
-    while(!img_path_read.eof())
+
+    if(generate_sample())
     {
-        getline(img_path_read, line);
-        if (line == "")
-            continue;
-        cur_img_pathes.push_back(line);
+        cout<<"Success Sample"<<endl;
+        return true;
     }
-    path_ = cur_img_pathes;
+    else
+    {
+        cout<<"Fail to Sample"<<endl;
+        return false;
+    }
+
+}
+
+//bool ConvertData::generate_positive()
+//{
+//    return generate_sample(1);
+//}
+//
+//bool ConvertData::generate_negative()
+//{
+//    return generate_sample(0);
+//}
+
+bool ConvertData::generate_sample()
+{
+    if(!txt_init())
+    {
+        cout<<"Cannot initialize txt file"<<endl;
+        return false;
+    }
+
+    if(!file_list_read())
+    {
+        cout<<"Cannot read file list"<<endl;
+        return false;
+    }
+
+    for(int i=0; i<file_list_.size(); i++)
+    {
+        cout<< "Processing " << i << " Sample" << endl;
+
+        if(!num_read(i))
+        {
+            cout<<"Cannot read num"<<endl;
+            return false;
+        }
+
+        if(!rect_read(i))
+        {
+            cout<<"Cannot read rect"<<endl;
+            return false;
+        }
+
+        if(!img_path_read(i))
+        {
+            cout<<"Cannot read img path"<<endl;
+            return false;
+        }
+
+//        if(!img_read(i))
+//        {
+//            cout<<"Cannot read img "<< cur_img_path_ << endl;
+//            return false;
+//        }
+
+//        if(!create_folder())
+//        {
+//            cout<<"Cannot read file list"<<endl;
+//            return false;
+//        }
+
+        if(!sample_write(i))
+        {
+            cout<<"Fail to Write Sample"<<endl;
+            return false;
+        }
+
+//        if(state == 1)
+//        {
+//            if (!positive_write(i)) {
+//                cout << "Fail to Positive Sample" << endl;
+//                return false;
+//            } else {
+//                cout << "Success Positive Sample" << endl;
+//                return true;
+//            }
+//        }
+//        else
+//        {
+//            if(!negative_write(i))
+//            {
+//                cout<<"Fail to Positive Sample"<<endl;
+//                return false;
+//            }
+//            else
+//            {
+//                cout<<"Success Positive Sample"<<endl;
+//                return true;
+//            }
+//        }
+    }
+
     return true;
 }
+
+
+bool ConvertData::file_list_read()
+{
+    return file_list_read(params_[0] + "name_list.txt");
+}
+
+bool ConvertData::file_list_read(string path)
+{
+    ifstream in(path);
+    while(!in.eof())
+    {
+        string line;
+        getline(in, line);
+        if(line == "")
+            continue;
+        file_list_.push_back(line);
+    }
+
+    if(file_list_.size() != 0)
+        return true;
+    else
+        return false;
+}
+
+bool ConvertData::num_read(int i)
+{
+    return num_read(params_[0] + file_list_[i] + "_head_num.txt");
+}
+
+bool ConvertData::num_read(string path)
+{
+    num_.clear();
+    ifstream num_file(path);
+    while(!num_file.eof())
+    {
+        string line;
+        getline(num_file, line);
+        if(line == "")
+            continue;
+        int cur_num = num_convert(line);
+        num_.push_back(cur_num);
+    }
+
+    if(num_.size() != 0)
+        return true;
+    else
+        return false;
+}
+
+int ConvertData::num_convert(string num)
+{
+    return atoi(num.c_str());
+}
+
+bool ConvertData::rect_read(int i)
+{
+    return rect_read(params_[0] + file_list_[i] + ".txt");
+}
+
+bool ConvertData::rect_read(string path)
+{
+    rect_.clear();
+    vector<Rect> rect;
+    int count = 0;
+    ifstream rect_file(path);
+    while(!rect_file.eof())
+    {
+        string line;
+        getline(rect_file, line);
+        if(line == "")
+            continue;
+        Rect cur_rect = rect_convert(line);
+        rect.push_back(cur_rect);
+        if(rect.size() == num_[count])
+        {
+            rect_.push_back(rect);
+            count++;
+            rect.clear();
+        }
+    }
+
+    if(rect_.size() != 0)
+        return true;
+    else
+        return false;
+}
+
+Rect ConvertData::rect_convert(string rect)
+{
+    char * cstr = new char[rect.length()+1];
+    strcpy(cstr, rect.c_str());
+    char * p = strtok(cstr, ",");
+    vector<float> rect_list;
+    while(p != 0)
+    {
+        float pos = atof(p);
+        rect_list.push_back(pos);
+        p = strtok(NULL, ",");
+    }
+    return Rect(rect_list[0], rect_list[1], rect_list[2], rect_list[3]);
+}
+
+bool ConvertData::img_path_read(int i)
+{
+    return img_path_read(params_[0] + file_list_[i] + "_img_path.txt");
+}
+
+bool ConvertData::img_path_read(string path)
+{
+    img_path_.clear();
+    ifstream img_path_file(path);
+    while(!img_path_file.eof())
+    {
+        string line;
+        getline(img_path_file, line);
+        if(line == "")
+            continue;
+        img_path_.push_back(line);
+    }
+
+    if(img_path_.size() != 0)
+        return true;
+    else
+        return false;
+}
+
+//bool ConvertData::img_read(int i)
+//{
+//    return img_read(params_[1] + file_list_[i]);
+//}
+//
+//bool ConvertData::img_read(string path)
+//{
+//    img_.clear();
+//    for(int i = 0; i < img_path_.size(); i++)
+//    {
+//        cur_img_path_ = path + img_path_[i];
+//        Mat cur_img = imread(cur_img_path_);
+//        if(cur_img.empty())
+//            return false;
+//        img_.push_back(cur_img);
+//    }
+//
+//    if(img_.size() != 0)
+//        return true;
+//    else
+//        return false;
+//}
 
 
 //Create one relative folder for the image
@@ -102,16 +294,80 @@ bool ConvertData::create_folder(string dir_path)
     }
 }
 
-vector<Mat> img_Mat_read(string headpath_img, int i)
+bool ConvertData::positive_write(int i)
+{
+    for(int j = 0; j < img_path_.size(); j++)
+    {
+        //read img
+        string cur_img_path = params_[1] + file_list_[i] + img_path_[j];
+        if(img_read(cur_img_path))
+        {
+            cout << "Cannot open " << cur_img_path << endl;
+            continue;
+        }
+
+        if(state_ == 1)
+        {
+            //create the folder for file list
+            cur_img_path = params_[2] + "1/" + file_list_[i];
+            create_folder(cur_img_path);
+        }
+        if(state_ == 0)
+        {
+            //create the folder for file list
+            cur_img_path = params_[2] + "0/" + file_list_[i];
+            create_folder(cur_img_path);
+        }
+
+        //create the folder for img
+        cur_img_path = params_[2] + file_list_[i] + img_path_[j];
+        create_folder(cur_img_path);
+
+        //create the folder for img
+        cur_img_path = params_[2] + file_list_[i] + img_path_[j];
+        create_folder(cur_img_path);
+
+        //write positve
+        for(int k = 0; k < num_[j]; k++)
+        {
+            positive_sample(cur_img_, rect_[j][k]);
+            img_write();
+        }
+    }
+
+}
+
+bool ConvertData::negative_write(int i)
 {
 
-    vector<Mat> cur_imgs;
-    Mat cur_img;
-    for(int j=0; j<afw_face_path_[i].size(); j++)
-    {
-        string img_path = headpath_img + mid_file_list_[i] + afw_face_path_[i][j];
-        cur_img = imread(img_path);
-        cur_imgs.push_back(cur_img);
-    }
-    return cur_imgs;
+}
+
+bool ConvertData::img_read(string path)
+{
+    cur_img_ = imread(path);
+    if(cur_img_.empty())
+        return false;
+    else
+        return true;
+}
+
+Mat ConvertData::positive_sample(Mat img, Rect rect)
+{
+    if(rect.x <= 0) rect.x = 0;
+    if(rect.y <= 0) rect.y = 0;
+    if(img.cols < (rect.x + rect.width)) rect.width = img.cols-rect.x;
+    if(img.rows < (rect.y + rect.height)) rect.height = img.rows - rect.y;
+    if(rect.width <= 0) rect.width = 0;
+    if(rect.height <= 0) rect.height = 0;
+    return img(rect);
+}
+
+
+bool ConvertData::txt_init()
+{
+    txtfile_.open(params_[0]);
+    if(txtfile_.is_open())
+        return true;
+    else
+        return false;
 }
--- ConvertData.h
+++ ConvertData.h
@@ -7,6 +7,8 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <boost/filesystem.hpp>
 
@@ -14,16 +16,81 @@ using namespace std;
 using namespace cv;
 
 class ConvertData {
-    vector<string> path_;
 
+public:
+	//Constructor function
+	ConvertData();
+	ConvertData(std::vector<string> params);
 
-    bool img_path_read(string headpath, int i);
+	//Deconstructor function
+    ~ConvertData();
 
-    //Create one relative folder for the image
-    bool create_folder(string dir_path);
+    //the interface function
+    bool run();
+
+    //the ture function generate sample
+    bool generate_sample();
 
     //generate the positive sample
     bool generate_positive();
+
+    //generate the negative sample
+    bool generate_negative();
+
+    //read the img path, face number, face rectangle and img
+    bool file_list_read();
+    bool file_list_read(string path);
+    bool img_path_read(int i);
+    bool img_path_read(string path);
+    bool num_read(int i);
+    bool num_read(string path);
+    bool rect_read(int i);
+    bool rect_read(string path);
+    bool img_read(int i);
+    bool img_read(string path);
+
+    //convert one element(path, num, rect, params) to the needed format
+    string img_path_convert(); //not need for this time
+    int num_convert(string num);
+    Rect rect_convert(string rect);
+
+    //write the positive sample or the negative sample
+    bool sample_write(int i);
+    bool positive_write(int i);
+    bool negative_write(int i);
+    bool img_write();
+    bool txt_write();
+    bool txt_init();
+
+    //Create one relative folder for the image
+    bool create_folder();
+    bool create_folder(string dir_path);
+
+
+    //variable
+public:
+    vector<string> params_;
+    vector<string> file_list_;
+    vector<int> num_;
+    vector<vector<Rect>> rect_;
+    vector<string> img_path_;
+    string cur_img_path_;
+    //vector<Mat> img_;
+    ofstream txtfile_;
+    int state_;
+    Mat cur_img_;
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
 
